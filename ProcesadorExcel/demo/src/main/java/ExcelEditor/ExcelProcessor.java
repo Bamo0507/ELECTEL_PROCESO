50539dcf479f80ea9cde49d2a41a23436a30f16b
Bamo0507 <bryanalbertomo@gmail.com>
2024-06-11 14:10:24 -0600

    Modificación

--- ProcesadorExcel/demo/src/main/java/ExcelEditor/ExcelProcessor.java
+++ ProcesadorExcel/demo/src/main/java/ExcelEditor/ExcelProcessor.java
@@ -77,10 +77,15 @@ public class ExcelProcessor {
                 String txtxFileName = txtxFile.getName();
                 String txtFileName = txtxFileName.substring(0, txtxFileName.length() - 1); // Eliminar la última 'x'
                 File txtFile = new File(exportFolder, txtFileName);
-                
+                if (txtxFile.renameTo(txtFile)) {
+                    System.out.println("Archivo corregido: " + txtFile.getName());
+                } else {
+                    System.out.println("Error al corregir el archivo: " + txtxFile.getName());
+                }
             }
         }
     }
+
 
 
     // Método para procesar cada archivo Excel
